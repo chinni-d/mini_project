3540a4a16f7f3a75a010c3325f74008b6499c49d
Darapureddy Manikanta <164158368+chinni-d@users.noreply.github.com>
2024-06-12 08:58:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function scrollToSkills() {
+    document.getElementById('skills-section').scrollIntoView({ behavior: 'smooth' });
+  }
+  function scrollToProjects() {
+    document.getElementById('projects-section').scrollIntoView({ behavior: 'smooth' });
+  }
+  function scrollToAbout() {
+    document.getElementById('About-section').scrollIntoView({ behavior: 'smooth' });
+  }
+  function scrollToContact() {
+    document.getElementById('contact-section').scrollIntoView({ behavior: 'smooth' });
+  }
